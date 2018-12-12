2db3b53403fc5236e4ce5b7efebcdee2257f165e
Andreas Stamminger <astamminger@users.noreply.github.com>
2018-12-12 12:19:46 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,17 @@
-SET DOC_BASE="%SRC_DIR%\docs\_theme\djangodocs\static"
-SET DOC_EPUB_BASE="%SRC_DIR%\docs\_theme\djangodocs-epub\static"
+SET DOC_BASE=%SRC_DIR%\docs\_theme\djangodocs\static
+SET DOC_EPUB_BASE=%SRC_DIR%\docs\_theme\djangodocs-epub\static
 echo "#########################################################################"
-echo "%DOC_BASE%
+echo %DOC_BASE%
+echo %DOC_EPUB_BASE%
 echo "#########################################################################"
 
 echo "Copy docicons-behindscenes.png"
-copy /Y "%DOC_BASE%\docicons-behindscenes.png" "%DOC_EPUB_BASE%\docicons-behindscenes.png"
+copy /Y %DOC_BASE%\docicons-behindscenes.png %DOC_EPUB_BASE%\docicons-behindscenes.png
 echo "Copy docicons-note.png"
-copy /Y "%DOC_BASE%\docicons-note.png" "%DOC_EPUB_BASE%\docicons-note.png"
+copy /Y %DOC_BASE%\docicons-note.png %DOC_EPUB_BASE%\docicons-note.png
 echo "Copy docicons-philosophy.png"
-copy /Y "%DOC_BASE%\docicons-philosophy.png" "%DOC_EPUB_BASE%\docicons-philosophy.png"
+copy /Y %DOC_BASE%\docicons-philosophy.png %DOC_EPUB_BASE%\docicons-philosophy.png
 echo "Copy docicons-warning.png"
-copy /Y "%DOC_BASE%\docicons-warning.png" "%DOC_EPUB_BASE%\docicons-warning.png"
+copy /Y %DOC_BASE%\docicons-warning.png %DOC_EPUB_BASE%\docicons-warning.png
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed .
