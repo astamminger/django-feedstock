52fa38831407abbcf0b4f5b62d9befbedff776d3
Andreas Stamminger <astamminger@users.noreply.github.com>
2018-12-12 13:01:15 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,12 +6,16 @@ echo %DOC_EPUB_BASE%
 echo "#########################################################################"
 
 echo "Copy docicons-behindscenes.png"
-copy /Y %DOC_BASE%\docicons-behindscenes.png %DOC_EPUB_BASE%\docicons-behindscenes.png
+del %DOC_EPUB_BASE%\docicons-behindscenes.png
+copy %DOC_BASE%\docicons-behindscenes.png %DOC_EPUB_BASE%\docicons-behindscenes.png
 echo "Copy docicons-note.png"
-copy /Y %DOC_BASE%\docicons-note.png %DOC_EPUB_BASE%\docicons-note.png
+del %DOC_EPUB_BASE%\docicons-note.png
+copy %DOC_BASE%\docicons-note.png %DOC_EPUB_BASE%\docicons-note.png
 echo "Copy docicons-philosophy.png"
-copy /Y %DOC_BASE%\docicons-philosophy.png %DOC_EPUB_BASE%\docicons-philosophy.png
+del %DOC_EPUB_BASE%\docicons-philosophy.png
+copy %DOC_BASE%\docicons-philosophy.png %DOC_EPUB_BASE%\docicons-philosophy.png
 echo "Copy docicons-warning.png"
-copy /Y %DOC_BASE%\docicons-warning.png %DOC_EPUB_BASE%\docicons-warning.png
+del %DOC_EPUB_BASE%\docicons-warning.png
+copy %DOC_BASE%\docicons-warning.png %DOC_EPUB_BASE%\docicons-warning.png
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed .
